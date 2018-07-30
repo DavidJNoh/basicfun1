0825cc5b98d7b6ec6d6ad90c50b8e4f2bada6b37
DavidJNoh <davidnohj@gmail.com>
2018-07-30 11:55:11 -0700

    wefa

---
+++ first.py
@@ -0,0 +1,23 @@
+# n = ['stop', 'asking', 'speros']
+# for element in n:
+#     print(element)
+    
+# a= 5
+# b= 10
+# def sum(a,b):
+#     print ("a:", a, "b:",b)
+#     return a+b
+
+# sum(a,b)
+
+# print(sum(a,b))
+
+# def say_hello(name="lfwjefl"):
+#     if name:
+#         print("hello, " + name + ", from inside the function")
+#     else:
+#         print('no name')
+
+
+# say_hello()
+
---
+++ for_loop_basic1.py
@@ -0,0 +1,38 @@
+# for count in range (0, 151):
+#     print(count)
+
+# for count in range (5,1000001):
+#     if count % 5 == 0:
+#         print(count)
+
+# for count in range (1,100):
+#     if count % 10 ==0:
+#         if count % 5 ==0:
+#             print ("Coding Dojo")
+#     elif count % 5==0:
+#         print("Coding")
+
+# numbers = range(0, 500001)
+# def addOddNumbers(numbers):
+#     total = 0
+#     for num in numbers:
+#         if num % 2 ==1:
+#             total += num
+#     print (total)
+
+# addOddNumbers(numbers)
+
+
+
+# def countdown():
+#     for num in range(2018,0,-4):
+#         print (num)
+
+# countdown()
+
+# def mult(y,z,a):
+#     for x in range (y,z+1):
+#         if x%a==0:
+#             print (x)
+
+# mult(2,9,3)
